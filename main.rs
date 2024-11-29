196f3f12f162ed9d5ce6f52305e5898a7871e653
Mathews Etiegni <etiegnim@gmail.com>
2024-11-30 00:18:32 +0300

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,62 @@
+use mlua::{Lua,Table,Value};
+use std::io::{self, Write};
+
+mod game_state;
+mod game_state::GameState;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    //Start of the luna runtime
+    let lua = Lua::new();
+
+    //Loading of the game scripts
+    lua.load_from_std_lib(mlue::StdLib::ALL)?;
+
+    //Load room and item definition from Lua
+    lua.load_from_filesystem("scripts/rooms.lua")?;
+    lua.load_from_filesystem("scripts/items.lua")?;
+
+    loop{
+        //Display current room description
+        display_room_description(&lua, &game_state)?;
+
+        //Prompt for player action
+        print!("\n>");
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        let command = input.trim().to_lowercase();
+
+        //Processing of player command through lua
+        let result = process_player_command(&lua, &mut game_state, &command)?;
+
+        if result == "quit"{
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn display_room_description(lua: &lua, game_state: &GameState) -> Result<(), mlua::Error>{
+    let globals = lua.globals();
+    let describe_room: mlua::Function = globals.get("describe_room")?;
+
+    let description: String = describe_room.call(game_state.current_room)?;
+    println!("{}", description);
+
+    Ok(())
+}
+
+fn process_player_command(
+lua: &Lua,
+game_state: &mut GameState,
+command: &str) -> Result<String, mlua::Error>{
+    let globals = lua.globals();
+    let process_command: mlua::Function = globals.get("process_command")?;
+
+    //calls lua function to handle the command
+    let result: String = process_command.call((command, game_state))?;
+
+    Ok(Result)
+}
